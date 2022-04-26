7aa0659a218c428aee9af440fa6ac7e8965d3668
santanam23 <mirmir11.ms@gmail.com>
2022-04-25 19:58:07 -0500

    fixed my error

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -13,7 +13,7 @@ const Header = () => {
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <Link to="/">
-          <h1>Houston Hobbies</h1>
+          <h1>Houston Volleyball Social</h1>
         </Link>
 
         <nav className="text-center">

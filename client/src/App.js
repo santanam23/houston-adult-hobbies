29fbfdc8f6c15e08b804a0724c8eeb6b28dd2106
santanam23 <mirmir11.ms@gmail.com>
2022-05-02 19:56:37 -0500

    error fix

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,6 @@ import { setContext } from '@apollo/client/link/context';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
-
 import Home from './pages/Home';
 import Login from './pages/Login';
 import NoMatch from './pages/NoMatch';
